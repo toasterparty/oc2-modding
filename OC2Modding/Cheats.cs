f1bc53e5e13ef9aec04cc61f77d5f7a2b4499d0d
toasterparty <toasterparty@derpymail.org>
2022-08-27 10:22:41 -0700

    More cheats

--- OC2Modding/Cheats.cs
+++ OC2Modding/Cheats.cs
@@ -14,10 +14,51 @@ namespace OC2Modding
 
         public static void Update()
         {
-            if (OC2Config.CheatsEnabled && Input.GetKeyDown(KeyCode.Delete))
+            if (!OC2Config.CheatsEnabled)
+            {
+                return;
+            }
+            
+            if (Input.GetKeyDown(KeyCode.Delete))
             {
                 SkipLevel = true;
             }
+            else if (Input.GetKeyDown(KeyCode.Keypad1))
+            {
+                FinishLevel(1);
+            }
+            else if (Input.GetKeyDown(KeyCode.Keypad2))
+            {
+                FinishLevel(2);
+            }
+            else if (Input.GetKeyDown(KeyCode.Keypad3))
+            {
+                FinishLevel(3);
+            }
+            else if (Input.GetKeyDown(KeyCode.Keypad3))
+            {
+                FinishLevel(4);
+            }
+        }
+
+        private static void FinishLevel(int stars)
+        {
+            ServerCampaignFlowController flowController = GameObject.FindObjectOfType<ServerCampaignFlowController>();
+            if (flowController == null)
+            {
+                OC2Modding.Log.LogWarning("Couldn't Skip Level due to not being able to find ServerCampaignFlowController");
+                return;
+            }
+
+            flowController.SkipLevel(stars);
+            GameLog.LogMessage($"Auto-completed current level with {stars}-Star score");
+        }
+
+        [HarmonyPatch(typeof(LoadingScreenFlow), nameof(LoadingScreenFlow.LoadScene))]
+        [HarmonyPrefix]
+        private static void LoadScene()
+        {
+            SkipLevel = false;
         }
 
         [HarmonyPatch(typeof(ServerCampaignFlowController), "OnSuccessfulDelivery")]
