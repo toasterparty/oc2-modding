1891ec49dafb1cc6709f1ff2295fe64f9dea74fb
toasterparty <toasterparty@derpymail.org>
2022-09-05 19:28:38 -0700

    fix init order

--- OC2Modding/ArchipelagoClient/ArchipelagoClient.cs
+++ OC2Modding/ArchipelagoClient/ArchipelagoClient.cs
@@ -227,8 +227,8 @@ namespace OC2Modding
                             File.WriteAllText(saveName, json);
                         }
 
-                        OC2Config.InitJson(OC2Helpers.getCustomSaveDirectory() + "OC2Modding.json"); // current inventory
                         OC2Config.InitJson(saveName); // starting inventory
+                        OC2Config.InitJson(OC2Helpers.getCustomSaveDirectory() + "OC2Modding.json"); // current inventory
                     }
                 }
 
