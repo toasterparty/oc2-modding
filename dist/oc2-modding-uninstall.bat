a7a74522557039e9fc1007bb832bd6fb1dccabfc
toasterparty <toasterparty@derpymail.org>
2022-09-08 14:50:21 -0700

    update uninstall

--- dist/oc2-modding-uninstall.bat
+++ dist/oc2-modding-uninstall.bat
@@ -3,8 +3,11 @@ del /f /q changelog.txt
 del /f /q winhttp.dll
 del /f /q doorstop_config.ini
 del /f /q leaderboard_scores.json
+del /f /q leaderboard_scores.csv
+del /f /q OC2Modding.json
 del /f /q output_log.txt
 del /f /q /s BepInEx
 del /f /q /s curl
 rmdir BepInEx /S /Q
 rmdir curl /S /Q
+del "%~f0"
