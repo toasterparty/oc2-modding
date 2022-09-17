ed4e84816e2a3f191ed784dbb57aab5b10c75884
toasterparty <toasterparty@derpymail.org>
2022-09-16 21:55:14 -0700

    Play wood pickup error on client

--- OC2Modding/IngredientCrates.cs
+++ OC2Modding/IngredientCrates.cs
@@ -9,17 +9,34 @@ namespace OC2Modding
             Harmony.CreateAndPatchAll(typeof(IngredientCrates));
         }
 
+        private static bool CanPickup(string name)
+        {
+            if (OC2Config.DisableWood && name == "DLC05_Wood")
+            {
+                OC2Helpers.PlayErrorSfx();
+                return false;
+            }
+
+            return true;
+        }
+
         [HarmonyPatch(typeof(ServerPickupItemSpawner), nameof(ServerPickupItemSpawner.CanHandlePickup))]
         [HarmonyPostfix]
-        private static void CanHandlePickup(ref PickupItemSpawner ___m_pickupItemSpawner, ref bool __result)
+        private static void CanHandlePickup_Server(ref PickupItemSpawner ___m_pickupItemSpawner, ref bool __result)
         {
-            if (OC2Config.DisableWood && __result)
+            if (__result)
             {
-                if (___m_pickupItemSpawner.m_itemPrefab.name == "DLC05_Wood")
-                {
-                    OC2Helpers.PlayErrorSfx();
-                    __result = false;
-                }
+                __result = CanPickup(___m_pickupItemSpawner.m_itemPrefab.name);
+            }
+        }
+
+        [HarmonyPatch(typeof(ClientPickupItemSpawner), nameof(ClientPickupItemSpawner.CanHandlePickup))]
+        [HarmonyPostfix]
+        private static void CanHandlePickup_Client(ref PickupItemSpawner ___m_pickupItemSpawner, ref bool __result)
+        {
+            if (__result)
+            {
+                __result = CanPickup(___m_pickupItemSpawner.m_itemPrefab.name);
             }
         }
     }
