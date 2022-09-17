15ec7d589a339d822ee6a512cf1133f9d6251aa4
toasterparty <toasterparty@derpymail.org>
2022-09-16 20:28:03 -0700

    Scale level time and order time in client

--- OC2Modding/CustomOrderLifetime.cs
+++ OC2Modding/CustomOrderLifetime.cs
@@ -10,40 +10,28 @@ namespace OC2Modding
             Harmony.CreateAndPatchAll(typeof(CustomOrderLifetime));
         }
 
-        private static int lastDlc = 0;
-        private static List<string> patchedLevels = new List<string>();
+        private static Dictionary<string, float> ScaledLevelTime = new Dictionary<string, float>();
 
-        [HarmonyPatch(typeof(GameSession), nameof(GameSession.GetGameModeServer))]
-        [HarmonyPrefix]
-        private static void GetGameModeServer(ref KitchenLevelConfigBase levelConfig)
+        private static float GetScaledTime(float inTime, string levelName)
         {
-            int currentDlc = OC2Helpers.GetCurrentDLCID();
-            if (lastDlc != currentDlc)
+            if (ScaledLevelTime.ContainsKey(levelName))
             {
-                patchedLevels.Clear();
+                /* The time is already cached */
+                return ScaledLevelTime[levelName];
             }
-            lastDlc = currentDlc;
 
-            if (patchedLevels.Contains(levelConfig.name))
+            /* Handle special case (Story 6-6) */
+            float time = inTime;
+            if (OC2Config.Custom66TimerScale != 1.0f && levelName.StartsWith("s_dynamic_stage_04"))
             {
-                return; // Only patch each level once per DLC
-            }
-            patchedLevels.Add(levelConfig.name);
-
-            levelConfig.m_orderLifetime = OC2Config.CustomOrderLifetime;
-
-            if (OC2Config.Custom66TimerScale != 1.0f && levelConfig.name.StartsWith("s_dynamic_stage_04"))
-            {
-                float time = levelConfig.GetRoundData().m_roundTimer;
                 time *= OC2Config.Custom66TimerScale;
                 time -= (time % 30);
                 time += 30;
-                levelConfig.GetRoundData().m_roundTimer = time;
             }
-            else if (OC2Config.LevelTimerScale != 1.0f && !OC2Helpers.IsDynamicLevel(levelConfig.name))
+            /* Otherwise Scale by global scale */
+            else if (OC2Config.LevelTimerScale != 1.0f && !OC2Helpers.IsDynamicLevel(levelName))
             {
-                float beforeTime = levelConfig.GetRoundData().m_roundTimer;
-                float time = beforeTime*OC2Config.LevelTimerScale;
+                time *= OC2Config.LevelTimerScale;
                 if ((time % 10) >= 5)
                 {
                     time += 10 - (time % 10);
@@ -52,11 +40,31 @@ namespace OC2Modding
                 {
                     time -= (time % 10);
                 }
-
-                levelConfig.GetRoundData().m_roundTimer = time;
-
-                OC2Modding.Log.LogInfo($"Scaled Level Time from {((int)beforeTime)/60}:{beforeTime%60} to {((int)time)/60}:{time%60}");
             }
+
+            ScaledLevelTime[levelName] = time; // Cache result
+            OC2Modding.Log.LogInfo($"Scaled Level Time from {((int)inTime)/60}:{inTime%60} to {((int)time)/60}:{time%60}");
+            return time;
+        }
+
+        private static void UpdateLevel(ref KitchenLevelConfigBase levelConfig)
+        {
+            levelConfig.m_orderLifetime = OC2Config.CustomOrderLifetime;
+            levelConfig.GetRoundData().m_roundTimer = GetScaledTime(levelConfig.GetRoundData().m_roundTimer, levelConfig.name);
+        }
+
+        [HarmonyPatch(typeof(GameSession), nameof(GameSession.GetGameModeServer))]
+        [HarmonyPrefix]
+        private static void GetGameModeServer(ref KitchenLevelConfigBase levelConfig)
+        {
+            UpdateLevel(ref levelConfig);
+        }
+
+        [HarmonyPatch(typeof(GameSession), nameof(GameSession.GetGameModeClient))]
+        [HarmonyPrefix]
+        private static void GetGameModeClient(ref KitchenLevelConfigBase levelConfig)
+        {
+            UpdateLevel(ref levelConfig);
         }
     }
 }
