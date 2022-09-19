b302c9b55c826174b1627945bd7365aa4fadae98
toasterparty <toasterparty@derpymail.org>
2022-09-19 11:43:08 -0700

    disallow running install script outside of game dir

--- dist/oc2-modding-uninstall.bat
+++ dist/oc2-modding-uninstall.bat
@@ -1,4 +1,9 @@
 @echo off
+
+if not exist Overcooked2.exe echo Error: Uninstall script is not in game folder
+if not exist Overcooked2.exe pause
+if not exist Overcooked2.exe exit 1
+
 del /f /q changelog.txt 
 del /f /q winhttp.dll
 del /f /q doorstop_config.ini
